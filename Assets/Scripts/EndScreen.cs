0d05f7a84e004fed1eeb86cea84f082c4d365153
Sanctusz <yohans89@gmail.com>
2020-02-12 21:05:55 +0000

    Level Changes Difficulty Decrease

---
+++ Assets/Scripts/EndScreen.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class EndScreen : MonoBehaviour
+{
+	public TextMeshProUGUI timerText;
+	GameObject timer;
+	bool winState;
+	// Start is called before the first frame update
+	void Start()
+	{
+		timer = GameObject.Find("Timer");
+		winState = timer.GetComponent<StartGameTimer>().winState;
+		if (winState)
+		{
+			timerText.text = "You Won!";
+		}
+		else
+		{
+			timerText.text = "Better Luck Next Time!";
+		}
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.R) || Input.GetKeyDown(KeyCode.Space))
+		{
+			winState = false;
+			timer.GetComponent<StartGameTimer>().currentTime = timer.GetComponent<StartGameTimer>().gameTimer;
+			SceneManager.LoadScene("Level 0");
+		}
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			StartCoroutine(WaitThenQuit());
+		}
+	}
+	IEnumerator WaitThenQuit()
+	{
+		// Quit Game After Timer to allow for Fade Out Effect
+		yield return new WaitForSeconds(0.5f);
+		Application.Quit();
+	}
+}
---
+++ Assets/Scripts/StartGameTimer.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class StartGameTimer : MonoBehaviour
+{
+	public float currentTime = 0f;
+	public float menuTimer = 10f;
+	public float gameTimer = 120f;
+	public TextMeshProUGUI timerText;
+	public bool winState;
+	private static StartGameTimer instance = null;
+	public static StartGameTimer Instance
+	{
+		get { return instance; }
+	}
+	// Start is called before the first frame update
+	void Start()
+	{
+		if (SceneManager.GetActiveScene().name == "MainMenu")
+		{
+			currentTime = menuTimer;
+		}
+		else
+		{
+			currentTime = gameTimer;
+		}
+	}
+	// Update is called once per frame
+	void Update()
+	{
+		if (currentTime <= 0 && SceneManager.GetActiveScene().name == "MainMenu")
+		{
+			SceneManager.LoadScene("Level 0");
+			currentTime = gameTimer;
+		}
+		if (SceneManager.GetActiveScene().name == "MainMenu")
+		{
+			currentTime -= 1 * Time.deltaTime;
+			timerText.text = "Game Starts In: " + Mathf.CeilToInt(currentTime).ToString();
+		}
+		if (SceneManager.GetActiveScene().name != "Main Menu" && SceneManager.GetActiveScene().name != "EndScreen")
+		{
+			currentTime -= 1 * Time.deltaTime;
+			print(currentTime);
+		}
+		if (currentTime <= 0 && SceneManager.GetActiveScene().name != "MainMenu" && SceneManager.GetActiveScene().name != "EndScreen")
+		{
+			SceneManager.LoadScene("EndScreen");
+		}
+	}
+	void Awake()
+	{
+		if (instance != null && instance != this)
+		{
+			Destroy(this.gameObject);
+			return;
+		}
+		else
+		{
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
