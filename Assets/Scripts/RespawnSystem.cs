847d473b18f40e85134b3ee59d1af4a019c179b3
Arnar Snaer <arnarsnaer98@gmail.com>
2020-02-12 18:38:08 +0000

    Respawn system added

---
+++ Assets/Scripts/RespawnSystem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnSystem : MonoBehaviour
+{
+    public GameObject RedPlayer;
+    public GameObject BluePlayer;
+    public Vector3 position1;
+    public Vector3 position2;
+
+    // Start is called before the first frame update
+
+    void OnTriggerEnter2D(Collider2D badItem)
+    {
+        Debug.Log("Touchie");
+        if ((badItem.tag == "RedPlayer"))
+        {
+           StartCoroutine(RespawnPlayer1());
+        }
+        if ((badItem.tag == "BluePlayer"))
+        {
+            Debug.Log("Touchie2");
+            StartCoroutine(RespawnPlayer2());
+        }
+    }
+
+    IEnumerator RespawnPlayer1()
+    {
+        RedPlayer.gameObject.SetActive(false);
+        yield return new WaitForSeconds(3);
+        RedPlayer.transform.position = position1;
+        RedPlayer.gameObject.SetActive(true);
+    }
+
+    IEnumerator RespawnPlayer2()
+    {
+        BluePlayer.gameObject.SetActive(false);
+        yield return new WaitForSeconds(3);
+        BluePlayer.transform.position = position2;
+        BluePlayer.gameObject.SetActive(true);
+    }
+}
