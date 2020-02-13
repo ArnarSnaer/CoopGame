4654af5511edd706ad0f370490a0a041af83ef2f
Sanctusz <yohans89@gmail.com>
2020-02-13 14:29:12 +0000

    spriterenderer

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,12 +15,10 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private Rigidbody2D playerRb;
     private Rigidbody2D platformRb;
+    private SpriteRenderer platformSr;
     private Rigidbody2D zrb;
     private Transform tf;
     private CircleCollider2D cc;
-
-    private Color baseColor;
-    private Color tempColor;
     private Vector3 baseSize;
     private Vector3 ghostSize;
 
@@ -41,6 +39,7 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         playerRb = playerObj.GetComponent<Rigidbody2D>();
         platformRb = platformObj.GetComponent<Rigidbody2D>();
+        platformSr = platformObj.GetComponent<SpriteRenderer>();
         zrb = rb.GetComponentInChildren<Rigidbody2D>();
         tf = GetComponent<Transform>();
     }
@@ -93,7 +92,7 @@ public class PlayerMovement : MonoBehaviour
         if (!isGhost)
         {
             // Sprite rotation
-            if (h > 0)      transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            if (h > 0) transform.eulerAngles = new Vector3(0f, 0f, 0f);
             else if (h < 0) transform.eulerAngles = new Vector3(0f, 180f, 0f);
 
             // normal movement
@@ -116,9 +115,9 @@ public class PlayerMovement : MonoBehaviour
             Vector2 tempVect1 = new Vector2(h, v);
             Vector2 moveVect1 = tempVect1 * speed;
             rb.velocity = moveVect1;
-            Color tmp = sr.color;
+            Color tmp = platformSr.color;
             tmp.a = 0.66f;
-            sr.color = tmp;
+            platformSr.color = tmp;
         }
 
         // Ghost Layer, freeze
@@ -126,9 +125,9 @@ public class PlayerMovement : MonoBehaviour
         {
             // layer 0 is default, can interact with each other
             // layer 8 is player
-            Color tmp = sr.color;
+            Color tmp = platformSr.color;
             tmp.a = 1f;
-            sr.color = tmp;
+            platformSr.color = tmp;
             gameObject.layer = 0;
             rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
         }
@@ -148,7 +147,7 @@ public class PlayerMovement : MonoBehaviour
 
         rb.gravityScale = 1;
         isGhost = false;
-        
+
         cooldownBool = false;
         StartCoroutine(Cooldown(0.5f));
     }
